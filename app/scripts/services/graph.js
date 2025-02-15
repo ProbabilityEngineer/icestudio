50e2e94664443156105ef146c49042a8976bd83f
Carlos Venegas <carlos@magnitude.es>
2025-02-15 20:15:39 +0100

    Automatic Workflow with error test 1/N

--- app/scripts/services/graph.js
+++ app/scripts/services/graph.js
@@ -1,5 +1,5 @@
 //== JSHINT rules / START
-/* global isClickOnVertex */
+/* global NNNNisClickOnVertex */
 //== JSHINT rules / END
 
 //----------------------------------------------------------------------------
